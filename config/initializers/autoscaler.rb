206ba4a1e91b2a5910c919bb53c6b43464cfa0e8
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2013-06-30 12:38:30 +0200

    Sigterm

--- config/initializers/autoscaler.rb
+++ config/initializers/autoscaler.rb
@@ -3,5 +3,5 @@
 #
 #
 #Delayed::Worker.max_attempts = 3
-#Delayed::Worker.raise_signal_exceptions = :term
+Delayed::Worker.raise_signal_exceptions = :term
 #Delayed::Backend::ActiveRecord::Job.send(:include, Delayed::AutoScaler::Scaler) if defined?(Delayed::Backend::ActiveRecord::Job)
