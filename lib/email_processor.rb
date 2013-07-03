a0b841db29f93a573b9d81bf82379a02804401e7
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2013-07-03 13:19:12 +0200

    Transaction around email save

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,11 +1,11 @@
 class EmailProcessor
   def self.process(email)
-    user = User.find_by(email: email.from)
-    if user
       ActiveRecord::Base.transaction do
-       incident = user.incidents.create!(name: email.subject)
-       interaction = incident.interactions.create!(content: email.body)
+        user = User.find_by!(email: email.from)
+        if user
+         incident = user.incidents.create!(name: email.subject)
+         incident.interactions.create!(content: email.body)
+        end
       end
-    end
   end
 end
