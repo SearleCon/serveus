ca691788cb8132c44a1d620d98c8e0d991b22e78
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2013-07-17 14:48:15 +0200

    PDF asset changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Serveus::Application.configure do
   config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//#{ENV['S3_BUCKET']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com"
 
   config.assets.prefix = "/serveus/assets"
 
