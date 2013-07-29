3910b468ea699961daf587041eeec68a00acd523
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2013-07-29 11:00:35 +0200

    Fix basket bug

--- lib/email_strategy.rb
+++ lib/email_strategy.rb
@@ -1,9 +1,6 @@
 class EmailStrategy < DecentExposure::Strategy
   delegate :current_user, to: :controller
 
-
-
-
   def resource
     instance = params[:email] ?  model.new(controller.send(:email_params)) :  model.new(from: current_user.email )
   end
